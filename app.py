f564f9bc89df99e8f112159d5218090e29758d95
Duntis <jonathan.pettersson2002@gmail.com>
2024-09-27 18:40:17 +0200

    My first try

---
+++ app.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("800x500")
+welcome_message = Label(root, text="Välkommen till Duntis´s budgetprogram!")
+welcome_message.pack()
+
+
+B = Button(root, text="")
+B.pack()
+
+
+
+root.mainloop()
