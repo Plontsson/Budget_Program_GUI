9341a511264a791cda07a7588c8f580c3b3aeb58
Duntis <jonathan.pettersson2002@gmail.com>
2024-09-28 18:55:56 +0200

    Update
    Added the basic buttons for the budget program. Still learning Tkinter.

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
 from tkinter import *
 
 root = Tk()
-root.geometry("800x500")
-welcome_message = Label(root, text="Välkommen till Duntis´s budgetprogram!")
+
+
+root.title("Duntis Budgetprogram!")
+root.geometry("800x400")
+welcome_message = Label(root, text="Välkommen till Duntis´s budgetprogram!", font= ("Times 20 bold"), )
 welcome_message.pack()
 
+#Button 1, add budgetpost
+B_1 = Button(root, text="Lägg till budgetpost")
+B_1.pack(fill=BOTH, expand=TRUE)
 
-B = Button(root, text="")
-B.pack()
+#Button 2 change budgetpost
 
+B_2 = Button(root, text="Ändra Kategori")
+B_2.pack(fill=BOTH, expand=TRUE)
+
+#Button 3 write budget
+
+B_3= Button(root, text="Skriv ut budget")
+B_3.pack(fill=BOTH, expand=TRUE)
+
+#Button 4 Exit program
+
+B_4 = Button(root, text="Avsluta program", command=exit)
+B_4.pack(fill=BOTH, expand=TRUE)
 
 
 root.mainloop()
